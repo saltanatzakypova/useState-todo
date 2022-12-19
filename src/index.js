207b8345879c1ff0944361ebdc59162592070e3d
Saltanat <zakypovasaltanat6@gmail.com>
2022-12-20 00:11:31 +0600

    First commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+// import styled, { createGlobalStyle } from "styled-components";
+
+// const Global = createGlobalStyle`
+// *{
+//     margin: 0;
+//     padding: 0;
+//     box-sizing: border-box;
+// }
+// `;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <>
+    {/* <Global></Global> */}
+    <App />
+  </>
+);
